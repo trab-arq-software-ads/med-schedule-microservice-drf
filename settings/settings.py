8668f82e8de3b7366e41f38e824509a1f6123dca
DamiaoFilho <damiaofilho0123@gmail.com>
2024-04-12 19:20:02 -0300

    config routes

--- settings/settings.py
+++ settings/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "med-schedule-drf",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "3214",
         "HOST": "127.0.0.1",
         "DATABASE_PORT": "5432",
         "OPTIONS": {
